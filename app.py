03e422ddfacb1f8b37c73ffaf71d23a29ced2d2d
root <root@ip-172-31-34-171.us-east-2.compute.internal>
2017-12-27 06:44:43 +0000

    Added Database support

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+app.config.update({"MYSQL_PASSWORD":"password"})
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
@@ -11,6 +14,17 @@ def test():
 @app.route('/page1')
 def load():
     return render_template('testpage.html',value="Value1",title="Title",listval=[1,2,3,4,5])
-
+@app.route('/msql')
+def msql():
+    #data=mysql.connect(passwd='password',user='root')
+    data=mysql.connection.cursor()
+    #dat=data.cursor()
+    value=[]
+    data.execute('SELECT * FROM db.test')
+    val=data.fetchall()
+    for row in val:
+        value.append(str(row.num))
+    return render_template('testpage.html',value="Value1",title="MSQL",listval=value)
+#    return val
 if __name__=="__main__":
    app.run(debug='true')
