47529e70253589431ce0f0a3fa5cab338d0a0606
Janakiraman <ramramu.jr@gmail.com>
2017-12-28 14:18:37 +0530

    failure

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class Flasktest(unittest.TestCase):
       def test_get(self):
           
           test=app.hello_world()
-          self.assertEquals(test,"Hello, World!")
+          self.assertEquals(test,"Hello, World\")
 
 if __name__=="__main__":
    unittest.main()
